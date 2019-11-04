ccd3cbfb32a21ad1ca8085bb297ea11599fe68d5
“vipinww” <vipin.yadav@wwindia.com>
2019-11-04 19:25:42 +0530

    Asycn Await Updated on Routes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,24 @@ const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
 
-app.post('/users',(req,res)=>{
+app.post('/users', async (req,res)=>{
     const user  = new User(req.body)
-    user.save().then(() => {
-        res.send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+   
+   // Using Async Await
+    try{
+    await user.save()
+    res.send(user)
+    }
+    catch(e){
+        res.send(e)
+    }
+     // Without Callback
+    // const user  = new User(req.body)
+    // user.save().then(() => {
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
 app.get('/users',(req,res)=>{
@@ -35,19 +46,33 @@ app.get('/users/:id',(req,res)=>{
     })
 })
 
-app.post('/tasks',(req,res)=>{
+app.post('/tasks', async (req,res)=>{
     const task = new Task(req.body)
-    task.save().then(()=>{
-           res.send(task)
-    }).catch((err)=>{
-            res.status(400).send(err)
-    })
+     try
+     {await task.save()
+      res.send(task)
+      }
+     catch(e){res.send(e)}
+
+    // task.save().then(()=>{
+    //        res.send(task)
+    // }).catch((err)=>{
+    //         res.status(400).send(err)
+    // })
 })
 
-app.get('/tasks',(req,res)=>{
-    Task.find().then((data)=>{res.send(data)}).catch((err)=>{
-        res.send(err)
-    })
+app.get('/tasks', async (req,res)=>{
+   try{
+      const tasks =  await Task.find({})
+       res.send(tasks)
+   }
+   catch(e){
+       res.send(e)
+   }
+
+    // Task.find().then((data)=>{res.send(data)}).catch((err)=>{
+    //     res.send(err)
+    // })
 })
 
  
