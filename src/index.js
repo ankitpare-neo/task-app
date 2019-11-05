73a0ccc5769b9a0c3d8d25db2e2a92a4897fa234
“vipinww” <vipin.yadav@wwindia.com>
2019-11-05 12:52:33 +0530

    Routes separted

--- src/index.js
+++ src/index.js
@@ -1,83 +1,16 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./router/user')
+const taskRouter = require('./router/task')
 const app = express()
-app.use(express.json())
+
 const port = process.env.PORT || 3000
 
-app.post('/users', async (req,res)=>{
-    const user  = new User(req.body)
-   
-   // Using Async Await
-    try{
-    await user.save()
-    res.send(user)
-    }
-    catch(e){
-        res.send(e)
-    }
-     // Without Callback
-    // const user  = new User(req.body)
-    // user.save().then(() => {
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-})
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
-app.get('/users', async (req,res)=>{
-    try{
-        const data = await User.find({})
-        res.send(data)
-    }
-    catch(e)
-{
-    res.send(e)
-}    
-})
 
-app.get('/users/:id',(req,res)=>{
-   
-    const id  = req.params.id
-    try{
-       const data = await User.findById(id)
-       res.send(data)
-    }
-    catch(e)
-    {
-        res.send(e)
-    }
-})
-
-app.post('/tasks', async (req,res)=>{
-    const task = new Task(req.body)
-     try
-     {await task.save()
-      res.send(task)
-      }
-     catch(e){res.send(e)}
-
-    // task.save().then(()=>{
-    //        res.send(task)
-    // }).catch((err)=>{
-    //         res.status(400).send(err)
-    // })
-})
-
-app.get('/tasks', async (req,res)=>{
-   try{
-      const tasks =  await Task.find({})
-       res.send(tasks)
-   }
-   catch(e){
-       res.send(e)
-   }
-
-    // Task.find().then((data)=>{res.send(data)}).catch((err)=>{
-    //     res.send(err)
-    // })
-})
 
  
 
---
+++ src/router/user.js
@@ -0,0 +1,86 @@
+const User = require('../models/user')
+const express = require('express')
+const router = new express.Router()
+
+router.post('/users', async (req,res)=>{
+    const user  = new User(req.body)
+   
+   // Using Async Await
+    try{
+    await user.save()
+    res.send(user)
+    }
+    catch(e){
+        res.send(e)
+    }
+     // Without Callback
+    // const user  = new User(req.body)
+    // user.save().then(() => {
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
+})
+
+router.get('/users', async (req,res)=>{
+    try{
+        const data = await User.find({})
+        res.send(data)
+    }
+    catch(e)
+{
+    res.send(e)
+}    
+})
+
+router.get('/users/:id', async (req,res)=>{
+   
+    const id  = req.params.id
+    try{
+       const data = await User.findById(id)
+       res.send(data)
+    }
+    catch(e)
+    {
+        res.send(e)
+    }
+})
+
+// Code to update the Data
+
+router.patch('/users/:id', async (req, res)=>{
+    const updates = Object.keys(req.body)
+    const allowUpdates = ['name','age','email','password']
+    const isValidOperation = updates.every((update)=> allowUpdates.includes(update))
+    if(!isValidOperation){
+        res.send({error:'Invalid Updates'})
+    }
+    const id = req.params.id;
+    try{
+    const updateUser = await User.findByIdAndUpdate(id,req.body,{new : true, runValidators : true})
+     if(!updateUser){
+         res.send("Data not found").status(404)
+     }
+     else{
+         res.send(updateUser)
+     }
+    }
+    catch(e){
+        res.send(e)
+    }
+
+})
+
+router.delete('/users/:id', async (req,res)=>{
+    const id = req.params.id
+    console.log(id)
+    try{
+    const task = await User.findByIdAndDelete(id)
+    res.send(task)
+    }
+    catch(e){
+        res.send(e) 
+    }
+})
+
+module.exports = router
