12cd030d096178b0936ebde0d4d9facaca8444d0
“vipinww” <vipin.yadav@wwindia.com>
2019-11-12 18:17:33 +0530

    Auth Token added

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-app.use((req,res,next)=>{ 
 
-    res.send("Under Maintainence")    
-})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -19,14 +17,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-// const jwt = require('jsonwebtoken')
-
-// const myFunction = async () => {
-//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-//     console.log(token)
-
-//     const data = jwt.verify(token, 'thisismynewcourse')
-//     console.log(data)
-// }
-
-// myFunction()
--- src/models/user.js
+++ src/models/user.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        trim: true,
-        unique : true
+        trim: true
     },
     email: {
         type: String,
-        required:true,
-        unique:true,         
+        unique: true,
+        required: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -41,24 +41,23 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
-    tokens:[{
-        token:{
+    tokens: [{
+        token: {
             type: String,
             required: true
         }
     }]
 })
 
-// Method to create token
-
-userSchema.method.generateToken = async function (){
+userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({_id: user._id.toString() }, 'myToken')
-    user.tokens = user.tokens.concat({token})
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
     await user.save()
+
     return token
 }
-
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
@@ -76,6 +75,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     return user
 }
 
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
@@ -85,7 +85,6 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
-
 
 const User = mongoose.model('User', userSchema)
 
